9d8288b5f50c2d599b84b94180976523df868ffc
unknown <sbshinobi@proton.me>
2025-02-19 01:45:02 +0530

    adding the test and building the app

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,10 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import App from '../App';
+import { expect } from 'vitest';
 test('should render App component', () => {
   render(<App />);
   screen.debug();
   const headerElement = screen.getByText(/Vite \+ React/i);
 
   expect(headerElement).toBeInTheDocument();
+});
+test('should render App component', () => {
+  render(<App />);
+  screen.debug();
+  const buttonElement = screen.getByText(/count is 0/i);
+  expect(buttonElement).toBeInTheDocument();
+  fireEvent(buttonElement, 'click');
+  expect(buttonElement).toHaveTextContent(/count is 1/i);
+  fireEvent(buttonElement, 'click');
+  expect(buttonElement).toHaveTextContent(/count is 2/i);
+  fireEvent(buttonElement, 'click');  
 });
