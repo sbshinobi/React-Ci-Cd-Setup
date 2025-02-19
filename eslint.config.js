48418fc6bb9cb1bd33457024dffaa5c9fe88e7e1
unknown <sbshinobi@proton.me>
2025-02-19 17:23:56 +0530

    added codes

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist','src/__tests__'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -17,9 +17,11 @@ export default [
         sourceType: 'module',
       },
     },
+ 
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
+      
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
